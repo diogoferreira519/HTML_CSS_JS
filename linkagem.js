539d36f8590034f188d6e2c54660fa858e640a84
Diogo Ferreira <diogoveigaferreira@hotmail.com>
2023-09-15 04:21:25 -0300

    Projeto inteiro

--- linkagem.js
+++ linkagem.js
@@ -1,11 +1,56 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
- */
-const buttonadd= document.querySelector('.caixa')
-const input = document.querySelector('.botao')
-function pegarOValorDoInput(){
-    console.log(input.value)
+
+const botao= document.querySelector('#botao ')
+const input = document.querySelector('#caixa')
+const listaTasks = document.querySelector('.lista')
+let listaItens = []
+botao.addEventListener('click', adicionaItens)
+function adicionaItens(){
+    if (input.value === '') {
+        alert("Você não pode inserir uma tarefa vazia");
+    } else{
+    listaItens.push({
+        tarefa: input.value,
+        concluida: false
+    })
+    input.value = ''
+    mostraItens()
+}
 }
 
-buttonadd.addEventListener('click', pegarOValorDoInput)
+function mostraItens() {
+    let newitem= ''
+
+    listaItens.forEach((task, index) => {
+        newitem = newitem + `
+        <li class="item ${task.concluida && "done"}">
+            <img src="checked.png" onclick ="concluidaTarefa(${index})" >
+            <p id ="textos">${task.tarefa}</p>
+            <img src="trash.png" onclick ="deletaritem(${index})">
+    </li>
+    `
+    })
+    listaTasks.innerHTML = newitem  
+    localStorage.setItem('lista', JSON.stringify(listaItens))
+}
+function deletaritem(indice){
+    listaItens.splice(indice, 1)
+    mostraItens()
+}
+
+function concluidaTarefa(posicao){
+    listaItens[posicao].concluida=!listaItens[posicao].concluida
+    mostraItens()
+    
+}
+function recarregarItens(){
+    const tarefasDoLocalStorage = localStorage.getItem('lista')
+    if(tarefasDoLocalStorage){
+
+        listaItens = JSON.parse(tarefasDoLocalStorage)
+    }
+    mostraItens()
+}
+recarregarItens()
+
+
+
